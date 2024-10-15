2e9bdf2d7c8a5e33b005702dcc9cde2faf782d2f
Zoltan Kovacs <zoltan@ikovacs.com>
2024-10-15 09:09:40 -0500

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,51 @@
+locals {
+
+  testbedconfig = <<TB
+***Disclaimer***
+By default, these templates store two critical files to the "examples" directory. DO NOT delete/lose these files:
+1. Terraform State file (terraform.tfstate) - Terraform must store state about your managed infrastructure and configuration. 
+   This state is used by Terraform to map real world resources to your configuration, keep track of metadata, and to improve performance for large infrastructures.
+   Terraform uses state to determine which changes to make to your infrastructure. 
+   Prior to any operation, Terraform does a refresh to update the state with the real infrastructure.
+   If this file is missing, you will NOT be able to make incremental changes to the environment resources without first importing state back to terraform manually.
+***Disclaimer***
+
+VPC:         
+${module.network.vpc_id}
+
+All CC AZs:
+${join("\n", module.cc_asg.availability_zone)}
+
+GWLB Endpoint Service Name:
+${module.gwlb_endpoint.vpce_service_name}
+
+GWLB Endpoint Service ARN:
+${module.gwlb_endpoint.vpce_service_arn}
+
+All GWLB Endpoint IDs:
+${join("\n", module.gwlb_endpoint.gwlbe)}
+
+GWLB:
+${module.gwlb.gwlb_arn}
+
+All CC IAM Role ARNs:
+${join("\n", module.cc_iam.iam_instance_profile_arn)}
+
+All Autoscaling Group IDs:
+${join("\n", module.cc_asg.autoscaling_group_ids)}
+
+Launch Template ID:
+${module.cc_asg.launch_template_id}
+
+TB
+}
+
+output "testbedconfig" {
+  description = "AWS Testbed results"
+  value       = local.testbedconfig
+}
+
+resource "local_file" "testbed" {
+  content  = local.testbedconfig
+  filename = "../testbed.txt"
+}
